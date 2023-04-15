a45e2da1533b908edc84c3cae4204590adf7e5ec
AF3MP4 <103211743+AF3MP4@users.noreply.github.com>
2023-04-16 00:03:12 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -21,5 +21,5 @@ class Config(object):
     PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
     PRO_USERS.append(OWNER_ID)
     MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://aflahk80:aflah12345@cluster0.h7rhn.mongodb.net/?retryWrites=true&w=majority")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001761282961"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001593625135"))
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
